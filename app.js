39c7607c33cac8ae8f7409fd3ef85ad7af88e118
Angelica Esposo <73009213+enjelesposo@users.noreply.github.com>
2021-05-20 18:39:44 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const quizData = [
+    {
+        question: '影響',
+        choiceA: 'えきまで',
+        choiceB: 'えんぴつ',
+        choiceC: 'えいきょう',
+        choiceD: 'かげきょう',
+        correct: 'C'
+    },{
+        question: '交換',
+        choiceA: 'こうかん',
+        choiceB: 'こうりつ',
+        choiceC: 'しゅうかん',
+        choiceD: 'こうこう',
+        correct: 'A'
+    },{
+        question: '上げる',
+        choiceA: 'あげる',
+        choiceB: 'さげる',
+        choiceC: 'こわげる',
+        choiceD: 'ささげる',
+        correct: 'A'
+    },{
+        question: '必要',
+        choiceA: 'かならず',
+        choiceB: 'ひつよう',
+        choiceC: 'するべき',
+        choiceD: 'ないよう',
+        correct: 'B'
+    },{
+        question: '考える',
+        choiceA: 'こえる',
+        choiceB: 'かえる',
+        choiceC: 'つたえる',
+        choiceD: 'かんがえる',
+        correct: 'D'
+    }
+
+];
